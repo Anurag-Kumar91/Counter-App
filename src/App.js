792752b84caa8a142ad1fa4748f0b155a560ae2a
viratanu1234 <viratanu1234@gmail.com>
2023-05-28 12:39:00 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-
+//WE use react hook 
 import React ,{useState} from 'react';
 
 
 function App() {
   const[count,setCount]=useState(10)
-   //Decrementing the count
+   //Decrementing the counting
   function decrementCount(){
     setCount(prevCount=>prevCount-1) 
   }
-  //Incrementing the count
+  //Incrementing the counting
   function incrementCount(){
     setCount(prevCount=>prevCount+1)
   }
-
+ //It return
   return (
     <>
     <button onClick={decrementCount}>-</button>
