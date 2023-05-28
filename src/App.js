658b52ca1fbd3bfc653b18e4bbe46559218f4afa
viratanu1234 <viratanu1234@gmail.com>
2023-05-28 12:23:26 +0530

    Counter app

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import React ,{useState} from 'react';
 
 function App() {
   const[count,setCount]=useState(10)
-
+   //Decrementing the count
   function decrementCount(){
-    setCount(prevCount=>prevCount-1)
+    setCount(prevCount=>prevCount-1) 
   }
+  //Incrementing the count
   function incrementCount(){
     setCount(prevCount=>prevCount+1)
   }
